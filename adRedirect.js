e5fa362707a7871202715520649f731bda977eab
Avideos-557 <kaleyharris557@gmail.com>
2025-01-21 16:21:21 +0100

    Add files via upload

---
+++ adRedirect.js
@@ -0,0 +1,17 @@
+let isEvenClick = localStorage.getItem('isEvenClick') === 'true';
+let shouldRedirect = localStorage.getItem('shouldRedirect') === 'true';
+
+document.addEventListener('click', (event) => {
+  isEvenClick = !isEvenClick; // Toggle even/odd click
+  localStorage.setItem('isEvenClick', isEvenClick.toString());
+
+  if (isEvenClick && shouldRedirect) {
+    // Redirect to ad website
+    window.location.href = 'https://www.profitablecpmrate.com/m5u0fm024?key=915f5541df37252209d1ab523c2cc8e5';
+    shouldRedirect = false;
+    localStorage.setItem('shouldRedirect', shouldRedirect.toString());
+  } else if (!isEvenClick) {
+    shouldRedirect = true;
+    localStorage.setItem('shouldRedirect', shouldRedirect.toString());
+  }
+});
